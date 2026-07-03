8253f23dfe548c065559395b37d6254782ddffe9
whoangel <whoangel.agl@gmail.com>
2026-07-03 00:46:39 -0600

    deploy: homelab config - ports 4003/4004, real MinIO creds, Flutter points to 192.168.1.21:4003

--- app/lib/config/app_config.dart
+++ app/lib/config/app_config.dart
@@ -4,4 +4,4 @@ class AppConfig {
   const AppConfig({required this.baseUrl});
 }
 
-const appConfig = AppConfig(baseUrl: 'http://192.168.1.13:4000');
+const appConfig = AppConfig(baseUrl: 'http://192.168.1.21:4003');
